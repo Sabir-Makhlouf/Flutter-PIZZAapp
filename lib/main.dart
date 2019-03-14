5a15dd0894c24cd967c613b472cd5248774aff18
Sabir Makhlouf <sabir.makhlouf@gmail.com>
2019-03-14 16:02:17 +0100

    1

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:sabirmakhlouf_pizzaapp/GUI/Home.dart';
+
+void main(List<String> args) {
+ runApp(new MaterialApp(
+
+    
+    home:new Home(), 
+    title:'SabirMakhlouf PizzaAPP',
+    ));
+
+ }
